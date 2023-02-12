6172e5853d9690713c343dbac97d9a0ed38ae3a5
homingjui <richard.93600@gmail.com>
2023-02-12 08:29:11 +0800

    6 wheel

--- MARS_ROVER/MARS_ROVER.ino
+++ MARS_ROVER/MARS_ROVER.ino
@@ -20,6 +20,13 @@ int pwm_R = 3;
 int in1_R = 25;
 int in2_R = 27;
 
+int pwm_Lm = 4;
+int in1_Lm = 28;
+int in2_Lm = 26;
+int pwm_Rm = 5;
+int in1_Rm = 29;
+int in2_Rm = 31;
+
 void setup() {
   Serial.begin(115200);
 
@@ -48,11 +55,15 @@ void setup() {
   pinMode(pwm_R, OUTPUT);
   pinMode(in1_R, OUTPUT);
   pinMode(in2_R, OUTPUT);
+  pinMode(pwm_Lm, OUTPUT);
+  pinMode(in1_Lm, OUTPUT);
+  pinMode(in2_Lm, OUTPUT);
+  pinMode(pwm_Rm, OUTPUT);
+  pinMode(in1_Rm, OUTPUT);
+  pinMode(in2_Rm, OUTPUT);
 
-  digitalWrite(in1_L, LOW);
-  digitalWrite(in2_L, HIGH);
-  digitalWrite(in1_R, LOW);
-  digitalWrite(in2_R, HIGH);
+  stop_motor();
+
 
 }
 
@@ -60,84 +71,17 @@ void loop() {
   get_sbus();
 
   //  controller_on = true;
-  //  for ( int i = 0; i < 4 ; i++) {
-  //    if (mystick[i] < -10) {
-  //      for ( int j = 0; j < 8 ; j++)
-  //        mystick[j] = 500;
-  //      controller_on = false;
-  //    }
-  //  }
 
-  //  get_t8s();
-  for (int i = 0; i < 10; i++) {
-    Serial.print(mystick[i]);
-    Serial.print(",");
-  }
-  Serial.println("");
+//  for (int i = 0; i < 10; i++) {
+//    Serial.print(mystick[i]);
+//    Serial.print(",");
+//  }
+//  Serial.println("");
 
-  //  if (mystick[4] > 500 - 10 and mystick[4] < 500 + 10 and controller_on) {
   if (mystick[4] > 500 ) {
-    int myspeed = map(mystick[2], 0, 1000, -255, 255);
-    analogWrite(pwm_L, abs(myspeed));
-    analogWrite(pwm_R, abs(myspeed));
-    if (myspeed > 0) {
-      digitalWrite(in1_L, LOW);
-      digitalWrite(in2_L, HIGH);
-      digitalWrite(in1_R, LOW);
-      digitalWrite(in2_R, HIGH);
-    }
-    else if (myspeed < 0) {
-      digitalWrite(in1_L, HIGH);
-      digitalWrite(in2_L, LOW);
-      digitalWrite(in1_R, HIGH);
-      digitalWrite(in2_R, LOW);
-    }
-
-    //    int myservoa = map(mystick[0], 0, 1000, 0, 360);
-    //    int myservob = map(mystick[0], 0, 1000, 360, 0);
-    //    //    Serial.print(myservoa);
-    //    //    Serial.print(",");
-    //    HCPCA9685.Servo(0, myservob);
-    //    HCPCA9685.Servo(1, myservoa);
-    //    HCPCA9685.Servo(2, myservob);
-    //    HCPCA9685.Servo(3, myservoa);
-
     int turning = map(mystick[0], -5, 1005, -90, 90);
-
-
-    if (mystick[7] < 500) set_motor((float)turning, 0, false);
-    else {
-      int myspeed = map(mystick[2], 0, 1000, -255, 255);
-      analogWrite(pwm_L, abs(myspeed));
-      analogWrite(pwm_R, abs(myspeed));
-      if (myspeed > 0) {
-        digitalWrite(in1_L, LOW);
-        digitalWrite(in2_L, HIGH);
-        digitalWrite(in1_R, HIGH);
-        digitalWrite(in2_R, LOW);
-      }
-      else if (myspeed < 0) {
-        digitalWrite(in1_L, HIGH);
-        digitalWrite(in2_L, LOW);
-        digitalWrite(in1_R, LOW);
-        digitalWrite(in2_R, HIGH);
-      }
-      set_motor((float)turning, 0, true);
-    }
+    float myspeed = map(mystick[2], 0, 1000, -100, 100);
+    set_motor((float)turning, myspeed, mystick[7] > 500);
   }
-  else {
-    digitalWrite(in1_L, HIGH);
-    digitalWrite(in2_L, HIGH);
-    digitalWrite(in1_R, HIGH);
-    digitalWrite(in2_R, HIGH);
-    analogWrite(pwm_L, 0);
-    analogWrite(pwm_R, 0);
-    HCPCA9685.Servo(0, 180);
-    HCPCA9685.Servo(1, 180);
-    HCPCA9685.Servo(2, 180);
-    HCPCA9685.Servo(3, 180);
-  }
-
-
-  //  Serial.println();
+  else stop_motor();
 }
--- MARS_ROVER/motor_control.ino
+++ MARS_ROVER/motor_control.ino
@@ -5,19 +5,51 @@ float d1 = 271; // distance in mm
 float d2 = -278;
 float d3 = 301;
 float d4 = 304;
+float d23 = (-d2 + d3) / 2;
 
-int offset0 = 5;
-int offset1 = -10;
-int offset2 = 0;
-int offset3 = 0;
+int offset0 = -15;
+int offset1 = -7;
+int offset2 = -15;
+int offset3 = -10;
 
-void set_motor(float ang, float myspeed ,bool o) {
+float max_r = d1 + ( d3 / tan(M_PI / 2));
+float max_v = sqrt(square(d23) + square(d1 + max_r)) / max_r;
+float norm_v = 255 / max_v;
+
+void stop_motor() {
+  digitalWrite(in1_L, HIGH);
+  digitalWrite(in2_L, HIGH);
+  digitalWrite(in1_R, HIGH);
+  digitalWrite(in2_R, HIGH);
+  digitalWrite(in1_Lm, HIGH);
+  digitalWrite(in2_Lm, HIGH);
+  digitalWrite(in1_Rm, HIGH);
+  digitalWrite(in2_Rm, HIGH);
+  analogWrite(pwm_L, 0);
+  analogWrite(pwm_R, 0);
+  analogWrite(pwm_Lm, 0);
+  analogWrite(pwm_Rm, 0);
+  HCPCA9685.Servo(0, 180);
+  HCPCA9685.Servo(1, 180);
+  HCPCA9685.Servo(2, 180);
+  HCPCA9685.Servo(3, 180);
+}
+
+float check_inf_div(float a, float b) {
+  if ( a == INFINITY && b == INFINITY) return 1;
+  if ( a == INFINITY && b != INFINITY) return INFINITY;
+  if ( a != INFINITY && b == INFINITY) return 0;
+  else return a / b;
+}
+
+void set_motor(float ang, float myspeed , bool o) {
   // ang : 0~90 deg
   // speed : 0~100 %
   // ang = w4 ang  / w2 ang
 
+  myspeed = myspeed / 100;
   ang = (ang / 180) * M_PI ;
-  float r = d1 + ( d3 / tan(abs(ang)));
+  float r = d1 + ( d3 / tan(fabs(ang)));
   if (o) r = 0;
 
   float ang1 = atan( d3 / ( r + d1) );
@@ -48,4 +80,63 @@ void set_motor(float ang, float myspeed ,bool o) {
   HCPCA9685.Servo(1, ang3 + offset1);
   HCPCA9685.Servo(2, ang4 + offset2);
   HCPCA9685.Servo(3, ang6 + offset3);
+
+  float V13 = check_inf_div( sqrt(square(d23) + square(d1 + r)) , r ) ;
+  float V2  = check_inf_div( r + d4 , r );
+  float V46 = check_inf_div( sqrt(square(d23) + square(r - d1)) , r ) ;
+  float V5  = check_inf_div( r - d4 , r );
+
+  //  Serial.println(ang);
+  //  Serial.println(r);
+  //  Serial.println(myspeed);
+  //  Serial.println(V13);
+  //  Serial.println(V2);
+  //  Serial.println(V46);
+  //  Serial.println(V5);
+  //  Serial.println( myspeed * norm_v * V13);
+  //  Serial.println( myspeed * norm_v * V2);
+  //  Serial.println( myspeed * norm_v * V46);
+  //  Serial.println( myspeed * norm_v * V5);
+  //  Serial.println();
+
+  if (o) {
+    analogWrite(pwm_L , 255 * fabs(myspeed));
+    analogWrite(pwm_R , 255 * fabs(myspeed));
+    analogWrite(pwm_Lm, 255 * fabs(myspeed));
+    analogWrite(pwm_Rm, 255 * fabs(myspeed));
+
+  } else {
+    if (ang >= 0) {
+      analogWrite(pwm_L , max( 0, fabs(myspeed) * norm_v * V13));
+      analogWrite(pwm_R , max( 0, fabs(myspeed) * norm_v * V2) );
+      analogWrite(pwm_Lm, max( 0, fabs(myspeed) * norm_v * V46));
+      analogWrite(pwm_Rm, max( 0, fabs(myspeed) * norm_v * V5) );
+    } else {
+      analogWrite(pwm_R , max( 0, fabs(myspeed) * norm_v * V13));
+      analogWrite(pwm_L , max( 0, fabs(myspeed) * norm_v * V2) );
+      analogWrite(pwm_Rm, max( 0, fabs(myspeed) * norm_v * V46));
+      analogWrite(pwm_Lm, max( 0, fabs(myspeed) * norm_v * V5) );
+    }
+  }
+
+  if (myspeed < 0) {
+    digitalWrite(in1_L, HIGH);
+    digitalWrite(in2_L, LOW);
+    digitalWrite(in1_R, HIGH);
+    digitalWrite(in2_R, LOW);
+    digitalWrite(in1_Lm, HIGH);
+    digitalWrite(in2_Lm, LOW);
+    digitalWrite(in1_Rm, HIGH);
+    digitalWrite(in2_Rm, LOW);
+  } else {
+    digitalWrite(in1_L, LOW);
+    digitalWrite(in2_L, HIGH);
+    digitalWrite(in1_R, LOW);
+    digitalWrite(in2_R, HIGH);
+    digitalWrite(in1_Lm, LOW);
+    digitalWrite(in2_Lm, HIGH);
+    digitalWrite(in1_Rm, LOW);
+    digitalWrite(in2_Rm, HIGH);
+  }
+
 }
